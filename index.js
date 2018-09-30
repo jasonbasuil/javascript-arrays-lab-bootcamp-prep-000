cc7d849d5fb0129224cfda0490ff63110dc0ca25
Jason Basuil <basuilj@gmail.com>
2018-09-30 20:00:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var newKittens = kittens
-  newKittens.pop()
-  return newKittens
+  return kittens.slice(kittens.length-1)
 }
 function removeFirstKitten(){
   return kittens.slice(1)
